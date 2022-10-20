97925e2ab33fdaaebd18094ff4020170969a2256
Domipoke <42298508+Domipoke@users.noreply.github.com>
2022-10-21 00:19:18 +0200

    upd

--- Math/Cos/Cos.js
+++ Math/Cos/Cos.js
@@ -1,20 +1,20 @@
 var elt = document.getElementById('calculator');
 
 function CosTaylor(max) {
-    /*if (max<0) return;
+    var eq = "\\sum_{n=0}^{"+max+"}\\frac{\\left(-1\\right)^{n}x^{\\left(2n\\right)}}{\\left(2n\\right)!}"
+    return eq
+}
+function largeCosTaylor(max) {
+    if (max<0) return;
     var eq=""
     var side="+"
-    for (var i=1;i<=max;i=i+2) {
+    for (var i=0;i<=max;i=i+2) {
         eq=eq+side+"\\frac{x^{"+i+"}}{"+i+"!}"
         if (side=="+") side="-"
         else side="+"
     }
-    return eq*/
-    var eq = "\\sum_{n=0}^{"+max+"}\\frac{\\left(-1\\right)^{n}x^{\\left(2n\\right)}}{\\left(2n\\right)!}"
     return eq
-   
 }
-
 function updateGraph(calculator,id,eq,start="y=") {
     var elt = document.getElementById('calculator');
     elt.innerHTML=""
@@ -23,7 +23,3 @@ function updateGraph(calculator,id,eq,start="y=") {
     calculator.setExpression({ id: id, latex: start+eq});
 }
     
-   function fix(s) {
-       var res=s
-       return res
-   }
--- Math/Sin/Sin.js
+++ Math/Sin/Sin.js
@@ -1,7 +1,11 @@
 var elt = document.getElementById('calculator');
 
 function SinTaylor(max) {
-    /*if (max<0) return;
+    var eq = "\\sum_{n=0}^{"+max+"}\\frac{\\left(-1\\right)^{n}x^{\\left(2n+1\\right)}}{\\left(2n+1\\right)!}"
+    return eq
+}
+function largeSinTaylor(max) {
+    if (max<0) return;
     var eq=""
     var side="+"
     for (var i=1;i<=max;i=i+2) {
@@ -9,20 +13,12 @@ function SinTaylor(max) {
         if (side=="+") side="-"
         else side="+"
     }
-    return eq*/
-    var eq = "\\sum_{n=0}^{"+max+"}\\frac{\\left(-1\\right)^{n}x^{\\left(2n+1\\right)}}{\\left(2n+1\\right)!}"
     return eq
 }
-
 function updateGraph(calculator,id,eq,start="y=") {
     var elt = document.getElementById('calculator');
     elt.innerHTML=""
     var calculator=Desmos.GraphingCalculator(elt)
     calculator.setExpression({id: "sin",latex:"y=\\sin(x)"})
     calculator.setExpression({ id: id, latex: start+eq});
-    }
-    
-   function fix(s) {
-       var res=s
-       return res
-   }
+}
